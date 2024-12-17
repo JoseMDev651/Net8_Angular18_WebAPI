c03a5b54b7686b33627c3c63d9fa7af1709d74c3
Jose Manuel Lopez <jmlopez651@outlook.com>
2024-12-17 12:06:44 +0100

    Components, Services

--- employee-app/src/app/Components/employee/employee.component.ts
+++ employee-app/src/app/Components/employee/employee.component.ts
@@ -1,17 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Employee } from '../../Models/employee';
+import { EmployeeService } from '../../Services/employee.service';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  @ViewChild('myModal') model: ElementRef | undefined;
+  employeeList: Employee[] = [];
+  empService = inject(EmployeeService);
+  employeeForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.setFormState();
+  }
+
+
   openModal() {
     const empModal = document.getElementById('myModal');
     if (empModal != null) {
       empModal.style.display = 'block';
     }
+  }
+
+  closeModal() {
+    if (this.model != null) {
+      this.model.nativeElement.style.display = 'none';
+    }
+  }
+
+  getEmployees() {
+    this.empService.getAllEmployees().subscribe((res) => {
+      this.employeeList = res;
+    })
+  }
+
+  setFormState() {
+    this.employeeForm = this.fb.group(
+      {
+        id: [0],
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required]],
+        mobile: ['', [Validators.required]],
+        employeeId: ['', [Validators.required]],
+        salary: ['', [Validators.required]],
+        status: [false, [Validators.required]]
+      }
+    )
+  }
+
+  onSubmit() {
+    console.log(this.employeeForm.value);
   }
 }
--- employee-app/src/app/Services/employee.service.ts
+++ employee-app/src/app/Services/employee.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Employee } from '../Models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
+  private apiUrl = 'https://localhost:7061/api/Employee'
   constructor() { }
+
+  http = inject(HttpClient);
+
+  getAllEmployees() {
+    return this.http.get<Employee[]>(this.apiUrl);
+  }
 }
